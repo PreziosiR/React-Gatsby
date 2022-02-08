181f978576ee521916e2ade94cd6c7f9ecba27cc
PreziosiR <rpreziosi00@gmail.com>
2022-02-08 11:38:54 +0100

    add images to website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return(
     <Layout pageTitle="Home Page">
       <p>Hello world !</p>
+      <StaticImage
+        alt="Kitten"
+        src='https://image.posterlounge.fr/img/products/630000/629822/629822_poster_l.jpg'/>
+      <StaticImage
+        alt='puppy'
+        src='../images/puppy.jpg'/>
     </Layout>  
   )
 }
